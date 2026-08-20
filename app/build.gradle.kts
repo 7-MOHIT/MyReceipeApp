7ab3fc1fca73511dc96a142cc2f7ef8fea3e2d8b
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-20 12:18:19 +0530

    adding the coil  dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.ktor.client.core)
