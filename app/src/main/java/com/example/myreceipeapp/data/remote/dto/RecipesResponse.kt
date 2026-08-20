e25a2069d86fd3828ba2006a2e342b6a456543ed
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-20 15:37:11 +0530

    now adding more data of same name from the json data from the api hitted in the browser.

--- app/src/main/java/com/example/myreceipeapp/data/remote/dto/RecipesResponse.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/dto/RecipesResponse.kt
@@ -1,5 +1,8 @@
 package com.example.myreceipeapp.data.remote.dto
 
 data class RecipesResponse(
-    val recipes : List<>
+    val recipes : List<>,
+    val total : Int,
+    val skip : Int,
+    val limit : Int
 )
