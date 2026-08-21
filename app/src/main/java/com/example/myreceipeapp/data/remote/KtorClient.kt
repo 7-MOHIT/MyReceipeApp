48dd687c535ecb6c76a422a6075b6131a994da4c
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-22 01:52:03 +0530

    adding the full ktor client to the app .

--- app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
@@ -4,10 +4,17 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 object KtorClient {
     val client = HttpClient(Android) {
-        install(ContentNegotiation) { json() }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
     }
-}
 
+}
