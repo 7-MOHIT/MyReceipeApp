af4ca1098d45199409ee9b6a2d0b15cced6ed575
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-21 22:53:41 +0530

    added the KtorClient in the KtorClient.kt file .

--- app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
@@ -1,4 +1,13 @@
 package com.example.myreceipeapp.data.remote
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+
 object KtorClient {
+    val client = HttpClient(Android) {
+        install(ContentNegotiation) { json() }
+    }
 }
+
