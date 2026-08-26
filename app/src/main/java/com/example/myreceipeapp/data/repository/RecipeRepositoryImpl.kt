da71055ba423ec30d580651096108e6190b0fdef
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-26 10:24:17 +0530

    we have to override the functions which we have created in our RecipeRepository.kt file , and add the return type to each overrided suspend fun.

--- app/src/main/java/com/example/myreceipeapp/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/myreceipeapp/data/repository/RecipeRepositoryImpl.kt
@@ -1,4 +1,15 @@
 package com.example.myreceipeapp.data.repository
 
-class RecipeRepositoryImpl {
+import com.example.myreceipeapp.data.remote.RecipeApiService
+import com.example.myreceipeapp.data.remote.dto.RecipeDTO
+import com.example.myreceipeapp.domain.RecipeRepository
+
+class RecipeRepositoryImpl(private val apiService: RecipeApiService) : RecipeRepository {
+    override suspend fun getAllRecipes(): List<RecipeDTO> {
+        return apiService.getAllRecipes().recipes
+    }
+    override suspend fun getRecipeById(id: Int): RecipeDTO {
+        return apiService.getRecipeById(id)
+    }
+
 }
