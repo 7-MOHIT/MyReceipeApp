ab03ab0294570fd2b87213a4eb37b7f238611434
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-26 09:45:51 +0530

    added another suspend function to get recipe by id when user clicks one specific recipe , it will be moved to a screen where all the details of the specific recipe will be shown , so we need to get the recipe by that id .

--- app/src/main/java/com/example/myreceipeapp/data/remote/RecipeApiService.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/RecipeApiService.kt
@@ -1,5 +1,6 @@
 package com.example.myreceipeapp.data.remote
 
+import com.example.myreceipeapp.data.remote.dto.RecipeDTO
 import com.example.myreceipeapp.data.remote.dto.RecipeResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -8,5 +9,8 @@ import io.ktor.client.request.get
 class RecipeApiService(private val client : HttpClient) {
     suspend fun getAllRecipes(): RecipeResponse{
         return client.get(urlString = "${KtorClient.BASE_URL}recipes").body()
+    }
+    suspend fun getRecipeById(id : Int) : RecipeDTO{
+        return client.get(urlString = "${KtorClient.BASE_URL}recipes/$id").body()
     }
 }
