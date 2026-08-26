e79359e16577f97a2b485564309ff27a5c2d6f00
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-26 13:07:12 +0530

    updated the base url .. bcs two slashes are not allowed, only one is needed. 1 slash is already added in BASE_URL , then  no need to add in the variable again , so use this method.

--- app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/KtorClient.kt
@@ -17,16 +17,16 @@ object KtorClient {
         }
     }
      const val BASE_URL = "https://dummyjson.com/"
-    const val PRODUCTS = "$BASE_URL/products"
-    const val CARTS = "$BASE_URL/carts"
-    const val USERS = "$BASE_URL/users"
-    const val POSTS = "$BASE_URL/posts"
-    const val COMMENTS = "$BASE_URL/comments"
-    const val QUOTES = "$BASE_URL/quotes"
-    const val TODOS= "$BASE_URL/todos"
-    const val RECIPE = "$BASE_URL/recipes"
-    const val IMAGE = "$BASE_URL/images"
-    const val AUTH = "$BASE_URL/auth"
+    const val PRODUCTS = "${BASE_URL}products"
+    const val CARTS = "${BASE_URL}carts"
+    const val USERS = "${BASE_URL}users"
+    const val POSTS = "${BASE_URL}posts"
+    const val COMMENTS = "${BASE_URL}comments"
+    const val QUOTES = "${BASE_URL}quotes"
+    const val TODOS= "${BASE_URL}todos"
+    const val RECIPE = "${BASE_URL}recipes"
+    const val IMAGE ="${BASE_URL}images"
+    const val AUTH = "${BASE_URL}auth"
 
 
 }
