9e3e58608923754670527b6f1a31c101e851b16d
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-29 15:49:37 +0530

    if the error comes from viewModel , then the error message will be shown and a retry button will be shown and on click , again fetchRecipe will be called from the viewModel.

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.example.myreceipeapp.persentation.screens.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -11,7 +16,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myreceipeapp.persentation.ViewModels.HomeViewModel
 import com.example.myreceipeapp.ui.theme.myOrange
@@ -41,6 +49,36 @@ fun HomeScreen(
         ) {
             when {
                 viewModel.isLoading -> LoadingIndicator()
+                viewModel.errorMessage != null -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = viewModel.errorMessage ?: "",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = myOrange
+                        )
+                        Spacer(modifier = Modifier.height(24.dp))
+                        Button(
+                            onClick = { viewModel.fetchRecipes() },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 24.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = myOrange,
+                                contentColor = Color.White
+                            )
+                        ) {
+                            Text(
+                                text = "Retry",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
             }
         }
     }
