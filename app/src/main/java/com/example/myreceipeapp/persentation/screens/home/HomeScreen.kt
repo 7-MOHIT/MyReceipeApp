fc893824c62a733af878c8a85ef88a908f054172
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-30 08:15:31 +0530

    calling the home screen in MainActivity.kt file
    and in HomeScreen.kt file., we made a third option when viewModel returns the data , then :
    1. isLoading
    2. error
    3. when data comes.
    
    the third one is imp.
    in this ,,,, we are using a lazyVerticalGrid
    in this ... max 2 items are shown at a time in a row
    
    inside the grid , there are three conditions. :
    1. show homeHeader
    2. show categories if they are more than 1
    3. show the sectionHeader , if all categories is selected, then all Recipes is shown , rest in other case, name of the category will be shown

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/home/HomeScreen.kt
@@ -1,28 +1,56 @@
 package com.example.myreceipeapp.persentation.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.MenuOpen
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MenuOpen
+import androidx.compose.material.icons.filled.Restaurant
+import androidx.compose.material.icons.filled.RestaurantMenu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myreceipeapp.persentation.ViewModels.HomeViewModel
 import com.example.myreceipeapp.ui.theme.myOrange
+import io.ktor.client.plugins.cache.storage.FileStorage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -79,10 +107,147 @@ fun HomeScreen(
                         }
                     }
                 }
+
+                else -> {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(
+                            16.dp,
+                        ),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        item(span = { GridItemSpan(currentLineSpan = maxLineSpan) }) {
+                            // will be shown always.
+                            HomeHeader()
+                        }
+                        if (viewModel.categories.size > 1) {
+                            //only if the no. of categories is greater than 1
+                            item(span = { GridItemSpan(currentLineSpan = maxLineSpan) }) {
+                                categorySection(
+                                    category = viewModel.categories,
+                                    selected = viewModel.selectedCategory,
+                                    onSelected = viewModel::onCategorySelected
+                                )
+                            }
+                        }
+                        item(span = { GridItemSpan(currentLineSpan = maxLineSpan) }) {
+                            // this will be shown always.
+                            SectionHeader(
+                                title =
+                                    if (viewModel.selectedCategory == "All") "All Recipes"
+                                    else viewModel.selectedCategory,
+                                icon = Icons.Default.Menu
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+@Composable
+fun categorySection(
+    category: List<String>,
+    selected: String,
+    onSelected: (String) -> Unit
+) {
+    Column() {
+        SectionHeader(
+            title = "Categories",
+            icon = Icons.Default.Restaurant
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(category) { category ->
+                val isSelected = selected == category
+                FilterChip(
+                    selected = isSelected,
+                    onClick = { onSelected(category) },
+                    label = {
+                        Text(
+                            text = category,
+                            fontWeight = if (isSelected) FontWeight.SemiBold
+                            else FontWeight.Normal
+                        )
+                    },
+                    shape = RoundedCornerShape(16.dp),
+                    colors = FilterChipDefaults.filterChipColors(
+                        selectedContainerColor = myOrange,
+                        selectedLabelColor = Color.White,
+                        labelColor = Color.DarkGray,
+                        containerColor = Color.White
+                    ),
+                    border = null
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SectionHeader(
+    title: String,
+    icon: ImageVector,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = myOrange,
+            modifier = Modifier.size(20.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+        )
+    }
+
+}
+
+@Composable
+fun HomeHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(color = myOrange.copy(alpha = 0.1f))
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(56.dp)
+                .background(
+                    color = myOrange,
+                    shape = RoundedCornerShape(16.dp)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                Icons.Default.RestaurantMenu,
+                contentDescription = "Restaurant Menu Icon",
+                tint = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Column() {
+            Text(
+                text = "Hello, Chef!!",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(text = "Find something delicious to Cook")
+        }
+    }
+}
+
 
 @Composable
 fun LoadingIndicator() {
