09566d416cef4805242b898f5df80321900102f3
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-31 19:32:02 +0530

    updated the recipes variable.... and set it as null in value. and make it nullable .
    also
    created a function for fetching  recipes acc. to their ids .
    
    i.e passing the id will return the recipe ... or its details. will be shown on the screen.

--- app/src/main/java/com/example/myreceipeapp/persentation/ViewModels/RecipeDetailViewModel.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/ViewModels/RecipeDetailViewModel.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myreceipeapp.data.remote.KtorClient
 import com.example.myreceipeapp.data.remote.RecipeApiService
 import com.example.myreceipeapp.data.remote.dto.RecipeDTO
 import com.example.myreceipeapp.data.repository.RecipeRepositoryImpl
 import com.example.myreceipeapp.domain.Repository.RecipeRepository
+import kotlinx.coroutines.launch
 
 class RecipeDetailViewModel : ViewModel() {
     private val repository: RecipeRepository = RecipeRepositoryImpl(
@@ -26,7 +28,22 @@ class RecipeDetailViewModel : ViewModel() {
         private set
 
     // now the recipes.. to fetch .. by default it is empty , but if data comes from server , then the data will be shown
-    var recipes by mutableStateOf<List<RecipeDTO>>(emptyList())
+    var recipes by mutableStateOf<RecipeDTO?>(null)
         private set
 
+    // function to fetching the recipe by only a id given to it.
+//    it will show all the details of the ... recipe whose id is passed to it.
+    fun fetchRecipeDetails(id: Int) {
+        isLoading = true
+        errorMessage = null
+        try {
+            viewModelScope.launch {
+                recipes = repository.getRecipeById(id)
+            }
+        } catch (e: Exception) {
+            errorMessage = e.message ?: "An Unexpected error occured"
+        } finally {
+            isLoading = false
+        }
+    }
 }
