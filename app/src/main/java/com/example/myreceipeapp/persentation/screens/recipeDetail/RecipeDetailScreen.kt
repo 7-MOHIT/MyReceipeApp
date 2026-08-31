a0f1888264630529262a80506992433d68b83812
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-08-31 19:15:44 +0530

    created a recipeDetailScreen, and initialized its parameters.

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/recipeDetail/RecipeDetailScreen.kt
@@ -0,0 +1,15 @@
+package com.example.myreceipeapp.persentation.screens.recipeDetail
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myreceipeapp.persentation.ViewModels.RecipeDetailViewModel
+
+@Composable
+fun RecipeDetailScreen(
+    recipeId: Int,
+    onBack: () -> Unit,
+    viewModel: RecipeDetailViewModel = viewModel(),
+
+    ) {
+
+}
