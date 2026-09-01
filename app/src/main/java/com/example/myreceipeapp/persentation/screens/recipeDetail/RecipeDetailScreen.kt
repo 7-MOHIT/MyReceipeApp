5ce960fe4fc0f4c478df2e2ec200c27f44be71e6
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-01 12:49:22 +0530

    making the scaffold in our RecipeDetailScreen.kt and calling the MyTopBar for topbar and now three conditions are there ; 1. error 2. isLoading 3 data showing
    1st and second is implemented here using errorMessage composable.

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/recipeDetail/RecipeDetailScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/recipeDetail/RecipeDetailScreen.kt
@@ -1,18 +1,31 @@
 package com.example.myreceipeapp.persentation.screens.recipeDetail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myreceipeapp.persentation.Components.ErrorMessage
+import com.example.myreceipeapp.persentation.Components.LoadingIndicator
 import com.example.myreceipeapp.persentation.ViewModels.RecipeDetailViewModel
+import com.example.myreceipeapp.ui.theme.myOrange
 
 @Composable
 fun RecipeDetailScreen(
@@ -25,31 +38,54 @@ fun RecipeDetailScreen(
     LaunchedEffect(recipeId) {
         viewModel.fetchRecipeDetails(id = recipeId)
     }
+    Scaffold(
+        topBar = {
+            MyTopBar(
+                title = "RECIPE DETAILS",
+                onBack,
+                icon = Icons.AutoMirrored.Filled.ArrowBack
+            )
+        }) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(color = myOrange.copy(alpha = 0.05f))
+
+        ) {
+            when {
+                viewModel.isLoading -> LoadingIndicator(1.dp)
+                viewModel.errorMessage != null -> ErrorMessage(
+                    viewModel = viewModel,
+                    errorMessage = viewModel.errorMessage,
+                    onRetry = { viewModel.fetchRecipeDetails(recipeId) }
+                )
+
+
+            }
+        }
+
+    }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopBar(
-    title: String,
-    onBackClick: () -> Unit,
-    icon: ImageVector
+    title: String, onBackClick: () -> Unit, icon: ImageVector
 ) {
     TopAppBar(
         title = {
             Text(
-                text = title,
-                fontWeight = FontWeight.Bold
+                text = title, fontWeight = FontWeight.Bold
             )
-        },
-        navigationIcon = {
+        }, navigationIcon = {
             IconButton(onClick = onBackClick) {
                 Icon(
-                    imageVector = icon,
-                    contentDescription = "Back"
+                    imageVector = icon, contentDescription = "Back"
                 )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
+        }, colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color.White,
             titleContentColor = Color.DarkGray,
             navigationIconContentColor = Color.DarkGray
