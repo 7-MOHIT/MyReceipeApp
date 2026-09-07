b614e331b2a46dacc4b808bd30d5a3f6b8739887
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-07 14:27:59 +0530

    created the CartMainScreen composable screen , and passing the navcontroller and viewModel to this file,

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartMainScreen/CartMainScreen.kt
@@ -0,0 +1,14 @@
+package com.example.myreceipeapp.persentation.screens.CartMainScreen
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+@Composable
+fun CartMainScreen(
+    navController: NavController,
+    viewModel: CartMainScreenViewModel = viewModel(),
+
+    ) {
+
+}
