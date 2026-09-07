fb2a248e6d2738b9c88a3ea658705d62e2112f65
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-07 14:17:42 +0530

    fetched the carts data from the dummyJson.com website , now i want to show this cart information in the screen

---
+++ app/src/main/java/com/example/myreceipeapp/data/remote/dto/Carts/Cart.kt
@@ -0,0 +1,27 @@
+package com.example.myreceipeapp.data.remote.dto.Carts
+
+data class Cart(
+    val discountedTotal: Double,
+    val id: Int,
+    val products: List<Product>,
+    val total: Double,
+    val totalProducts: Int,
+    val totalQuantity: Int,
+    val userId: Int
+)
+data class CartResponse(
+    val carts: List<Cart>,
+    val limit: Int,
+    val skip: Int,
+    val total: Int
+)
+data class Product(
+    val discountPercentage: Double,
+    val discountedTotal: Double,
+    val id: Int,
+    val price: Double,
+    val quantity: Int,
+    val thumbnail: String,
+    val title: String,
+    val total: Double
+)
