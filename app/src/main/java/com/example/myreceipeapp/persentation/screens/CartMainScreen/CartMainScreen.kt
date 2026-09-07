11b2d52e0a69aba5af206712d9f70726e1dad8d9
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-07 18:35:40 +0530

    adding the launchedEffect and scaffold to the CartmainScreen.kt

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/CartMainScreen/CartMainScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartMainScreen/CartMainScreen.kt
@@ -1,18 +1,24 @@
 package com.example.myreceipeapp.persentation.screens.CartMainScreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -20,21 +26,24 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.myreceipeapp.data.remote.dto.Carts.Cart
 import com.example.myreceipeapp.persentation.Navigation.CartScreenRoute
 import com.example.myreceipeapp.persentation.Navigation.HomeRoute
 import com.example.myreceipeapp.persentation.Navigation.ProductMainScreenRoute
-import com.example.myreceipeapp.persentation.screens.home.SearchableTopAppBar
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CartMainScreen(
     navController: NavController,
     viewModel: CartMainScreenViewModel = viewModel(),
-
-    ) {
+) {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
+    LaunchedEffect(Unit) {
+        viewModel.fetchCart(userId = 1)
+    }
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
@@ -86,7 +95,38 @@ fun CartMainScreen(
             }
         }
     ) {
+        Scaffold(
+            topBar = { TopAppBar(title = { Text(text = "Carts") }) }
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier.padding(
+                    innerPadding
+                )
+            ) {
 
+                Text(text = "Cart Screen.")
+
+                when {
+                    viewModel.isLoading -> Text("Loading...")
+                    viewModel.errorMessage != null -> Text("Error: ${viewModel.errorMessage}")
+                    else -> {
+                        val carts = viewModel.cartItems
+                        Text("Cart count: ${carts.size}")
+                        LazyColumn {
+                            items(carts, key = { it.id }) { cart ->
+                                CartCard(cart)
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
     }
 
+}
+
+@Composable
+fun CartCard(cart: Cart) {
+    Text(text = "${cart.id}")
 }
