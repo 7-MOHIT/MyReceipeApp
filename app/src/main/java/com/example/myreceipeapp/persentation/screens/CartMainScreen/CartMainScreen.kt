a17346a1260be24c22ee31fb6dc7a937774257b1
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-08 14:16:15 +0530

    implemented the search feature in the CartMainScreen.kt

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/CartMainScreen/CartMainScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartMainScreen/CartMainScreen.kt
@@ -1,12 +1,15 @@
 package com.example.myreceipeapp.persentation.screens.CartMainScreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -27,9 +30,11 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.myreceipeapp.data.remote.dto.Carts.Cart
+import com.example.myreceipeapp.persentation.Components.SearchableTopAppBar
 import com.example.myreceipeapp.persentation.Navigation.CartScreenRoute
 import com.example.myreceipeapp.persentation.Navigation.HomeRoute
 import com.example.myreceipeapp.persentation.Navigation.ProductMainScreenRoute
+import com.example.myreceipeapp.ui.theme.myOrange
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -96,7 +101,15 @@ fun CartMainScreen(
         }
     ) {
         Scaffold(
-            topBar = { TopAppBar(title = { Text(text = "Carts") }) }
+            topBar = {
+                SearchableTopAppBar(
+                    title = "CARTS",
+                    isSearchActive = viewModel.isSearchActive,
+                    searchQuery = viewModel.searchQuery,
+                    onSearchQueryChange = viewModel::onSearchQueryChange,
+                    onSearchToggle = viewModel::onSearchToggle,
+                    onMenuClick = { scope.launch { drawerState.open() } })
+            }
         ) { innerPadding ->
             Column(
                 modifier = Modifier.padding(
@@ -128,5 +141,16 @@ fun CartMainScreen(
 
 @Composable
 fun CartCard(cart: Cart) {
-    Text(text = "${cart.id}")
+    Card(
+        modifier = Modifier.padding(12.dp),
+        colors = CardDefaults.cardColors(containerColor = myOrange)
+    ) {
+        Column() {
+            Text(text = "${cart.totalQuantity}")
+            Text(text = "${cart.totalProducts}")
+            Text(text = "${cart.products}")
+            Text(text = "${cart.id}")
+            Text(text = "${cart.userId}")
+        }
+    }
 }
