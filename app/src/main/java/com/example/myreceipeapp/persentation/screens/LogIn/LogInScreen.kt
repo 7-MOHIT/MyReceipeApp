aad26262e3ce50972f66afff2276fc1b1f16c55a
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-10 11:32:27 +0530

    created  these two files, LogInScreen.kt and SignUpScreen.kt

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInScreen.kt
@@ -0,0 +1,8 @@
+package com.example.myreceipeapp.persentation.screens.LogIn
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LogInScreen (){
+
+}
---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/SignUp/SignUpScreen.kt
@@ -0,0 +1,8 @@
+package com.example.myreceipeapp.persentation.screens.SignUp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignUpScreen() {
+
+}
