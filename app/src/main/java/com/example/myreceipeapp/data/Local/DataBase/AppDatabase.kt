d41c2e5a56983eaf1e46ccacd7ab913fede0e632
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-10 11:30:28 +0530

    created the room database in the AppDatabase.kt file and naming the db as "recipe_app_database"
    giving it the recipeDao as a abstract function.

--- app/src/main/java/com/example/myreceipeapp/data/Local/DataBase/AppDatabase.kt
+++ app/src/main/java/com/example/myreceipeapp/data/Local/DataBase/AppDatabase.kt
@@ -1,5 +1,38 @@
 package com.example.myreceipeapp.data.Local.DataBase
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.myreceipeapp.data.Local.DAO.RecipeDao
+import com.example.myreceipeapp.data.Local.ENTITY.RECIPE.RecipeEntity
+import com.example.myreceipeapp.data.Local.convertor.Convertors
+
 //its a database class.
-class AppDatabase {
+@Database(
+    entities = [RecipeEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Convertors::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: android.content.Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: databaseBuilder(
+                    context.applicationContext,
+                    klass = AppDatabase::class.java,
+                    name = "recipe_app_database"
+                ).build().also { INSTANCE = it }
+            }
+        }
+
+
+    }
 }
