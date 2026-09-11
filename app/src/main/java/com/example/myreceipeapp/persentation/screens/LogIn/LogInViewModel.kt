de40192c3c3f4bad7a884b1f01dab3361f0248ee
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-11 08:38:15 +0530

    now lets create the viewModel of logInScreen. LogInScreenViewModel.kt

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.myreceipeapp.persentation.screens.LogIn
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
+
+class LogInViewModel : ViewModel() {
+
+}
