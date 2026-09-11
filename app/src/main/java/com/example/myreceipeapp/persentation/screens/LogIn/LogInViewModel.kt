a68ea0757e8bffd948fe71910dc9d8eb86607edc
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-11 08:45:10 +0530

    created a log in function using ui state. means getting the email and password from the ui state.

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInViewModel.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInViewModel.kt
@@ -3,6 +3,7 @@ package com.example.myreceipeapp.persentation.screens.LogIn
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 
 data class LoginUiState(
@@ -14,7 +15,40 @@ data class LoginUiState(
     val isLoading: Boolean = false,
     val isLoginSuccessful: Boolean = false
 )
+
 class LogInViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState
+
+    fun onEmailChange(value: String) {
+        _uiState.update { it.copy(email = value, emailError = null, generalError = null) }
+    }
+
+    fun onPasswordChange(value: String) {
+        _uiState.update { it.copy(password = value, passwordError = null, generalError = null) }
+    }
+    fun login() {
+        val state = _uiState.value
+        val emailError = if (!android.util.Patterns.EMAIL_ADDRESS.matcher(
+                state.email).matches()) {
+            "Enter a valid email"
+        } else {
+            null
+        }
+        val passwordError = if (state.password.length < 6) {
+            "Password must be at least 6 characters"
+        } else {
+            null
+        }
+
+        if (emailError != null || passwordError != null) {
+            _uiState.update {
+                it.copy(
+                    emailError = emailError,
+                    passwordError = passwordError
+                )
+            }
+            return
+        }
+    }
 }
