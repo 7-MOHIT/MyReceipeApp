d6f90400aa8423c321cb955beb2c2db60d705179
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-11 08:39:31 +0530

    inside the viewModel , lets create a ui state, then calling it in our vM

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInViewModel.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/LogIn/LogInViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.myreceipeapp.persentation.screens.LogIn
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 
-
+data class LoginUiState(
+    val email: String = "",
+    val password: String = "",
+    val emailError: String? = null,
+    val passwordError: String? = null,
+    val generalError: String? = null,
+    val isLoading: Boolean = false,
+    val isLoginSuccessful: Boolean = false
+)
 class LogInViewModel : ViewModel() {
-
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState
 }
