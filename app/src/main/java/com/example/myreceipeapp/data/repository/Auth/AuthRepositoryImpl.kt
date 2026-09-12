fe97776ff19649c2b056ec1e74073f81c33f697f
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-12 16:52:44 +0530

    making the implementation of authRepository.kt which is in the domain layer, making this file of  implementations  as AuthRepositoryImpl.kt  in the data layer , and overriding all the four functions . signUp , logIn, logOut , getcurrentUser.

--- app/src/main/java/com/example/myreceipeapp/data/repository/Auth/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/myreceipeapp/data/repository/Auth/AuthRepositoryImpl.kt
@@ -1,4 +1,54 @@
 package com.example.myreceipeapp.data.repository.Auth
 
-class AuthRepositoryImpl {
+import com.example.myreceipeapp.domain.Repository.Auth.AuthRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : AuthRepository {
+
+    override suspend fun signUp(email: String, password: String): Result<FirebaseUser> {
+        return try {
+            val result = firebaseAuth.createUserWithEmailAndPassword(
+                email,
+                password
+            ).await()
+            result.user?.let { Result.success(it) }
+                ?: Result.failure(Exception("Sign up failed, user is null"))
+        } catch (e: Exception) {
+            Result.failure(
+                e
+            )
+        }
+    }
+
+    override suspend fun login(email: String, password: String): Result<FirebaseUser> {
+        return try {
+            val result = firebaseAuth.signInWithEmailAndPassword(
+                email,
+                password
+            ).await()
+            result.user?.let {
+                Result.success(
+                    it
+                )
+            }
+                ?: Result.failure(
+                    Exception(
+                        "Login failed, user is null"
+                    )
+                )
+        } catch (e: Exception) {
+            Result.failure(
+                e
+            )
+        }
+    }
+
+    override fun logout() = firebaseAuth.signOut()
+
+    override fun getCurrentUser(): FirebaseUser? = firebaseAuth.currentUser
 }
