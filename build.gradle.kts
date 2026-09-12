49c73bc61cc30bd8fa96c619227004bcf988c461
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-12 18:39:16 +0530

    adding the dependency od dagger hilt in the build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
