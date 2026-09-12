e086af2f7aa75d853a4b5779cde6b314a75ad3b6
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-12 16:57:21 +0530

    making the AuthModule.kt and adding the provideFirebaseAuth provideAuthRepository functions in it.

--- app/src/main/java/com/example/myreceipeapp/DI/AuthModule.kt
+++ app/src/main/java/com/example/myreceipeapp/DI/AuthModule.kt
@@ -1,4 +1,24 @@
 package com.example.myreceipeapp.DI
 
-class AuthModule {
+import com.example.myreceipeapp.data.repository.Auth.AuthRepositoryImpl
+import com.example.myreceipeapp.domain.Repository.Auth.AuthRepository
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(firebaseAuth: FirebaseAuth): AuthRepository =
+        AuthRepositoryImpl(firebaseAuth)
 }
