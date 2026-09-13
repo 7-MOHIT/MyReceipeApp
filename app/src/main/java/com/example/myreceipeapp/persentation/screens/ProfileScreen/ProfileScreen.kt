a6a7774a3ff5e6cdaf8fb56c8daba705accb5d1f
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-13 10:18:20 +0530

    adding the viewModel and calling the logOut fun in a button inside the profile screen.

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/ProfileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/ProfileScreen/ProfileScreen.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -23,19 +26,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.room.util.TableInfo
-import com.example.myreceipeapp.persentation.Components.SearchableTopAppBar
 import com.example.myreceipeapp.persentation.Navigation.CartScreenRoute
 import com.example.myreceipeapp.persentation.Navigation.HomeRoute
 import com.example.myreceipeapp.persentation.Navigation.ProductMainScreenRoute
 import com.example.myreceipeapp.persentation.screens.home.DrawerTop
-import com.example.myreceipeapp.ui.theme.myOrange
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileScreen(navController: NavController) {
+fun ProfileScreen(
+    profileViewModel: ProfileViewModel = hiltViewModel(),
+    navController: NavController,
+    onLogout: () -> Unit
+) {
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     ModalNavigationDrawer(
@@ -57,7 +62,8 @@ fun ProfileScreen(navController: NavController) {
                     onClick = {
                         scope.launch {
                             navController.navigate(HomeRoute)
-                            drawerState.close() }
+                            drawerState.close()
+                        }
                     },
                     modifier = Modifier.padding(horizontal = 12.dp)
                 )
@@ -84,6 +90,7 @@ fun ProfileScreen(navController: NavController) {
                     modifier = Modifier.padding(horizontal = 12.dp)
                 )
 
+
             }
         }
     ) {
@@ -105,9 +112,20 @@ fun ProfileScreen(navController: NavController) {
                     )
             }
         ) { innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding)) { }
-
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Button(
+                    onClick = {
+                        profileViewModel.logout()
+                        onLogout()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.error
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Logout")
+                }
+            }
         }
-
     }
 }
