b198e940f047eed04bcdf830396f9b8a2cfc8ff1
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-13 09:46:16 +0530

    adding the name to display it in the sideDrawer and in the profile screen
    so adding the name as a parameter in the AuthRepository.kt

--- app/src/main/java/com/example/myreceipeapp/domain/Repository/Auth/AuthRepository.kt
+++ app/src/main/java/com/example/myreceipeapp/domain/Repository/Auth/AuthRepository.kt
@@ -3,7 +3,7 @@ package com.example.myreceipeapp.domain.Repository.Auth
 import com.google.firebase.auth.FirebaseUser
 
 interface AuthRepository {
-    suspend fun signUp(email: String, password: String): Result<FirebaseUser>
+    suspend fun signUp(name: String, email: String, password: String): Result<FirebaseUser>
     suspend fun login(email: String, password: String): Result<FirebaseUser>
     fun logout()
     fun getCurrentUser(): FirebaseUser?
