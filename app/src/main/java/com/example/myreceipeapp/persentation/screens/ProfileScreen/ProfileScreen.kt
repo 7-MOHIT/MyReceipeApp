af78f6763d1907407fd7a54b21d26226602903c1
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-13 09:23:23 +0530

    made the profile  screen , added the scaffold in this , adding the navigation icon , and that SideDrawer.

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/ProfileScreen/ProfileScreen.kt
@@ -0,0 +1,111 @@
+package com.example.myreceipeapp.persentation.screens.ProfileScreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.room.util.TableInfo
+import com.example.myreceipeapp.persentation.Components.SearchableTopAppBar
+import com.example.myreceipeapp.persentation.Navigation.CartScreenRoute
+import com.example.myreceipeapp.persentation.Navigation.ProductMainScreenRoute
+import com.example.myreceipeapp.persentation.screens.home.DrawerTop
+import com.example.myreceipeapp.ui.theme.myOrange
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(navController: NavController) {
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(modifier = Modifier.fillMaxWidth(0.65f)) {
+                Spacer(modifier = Modifier.height(12.dp))
+                DrawerTop(navController);
+
+                HorizontalDivider(modifier = Modifier.padding(bottom = 2.dp))
+
+                NavigationDrawerItem(
+                    label = {
+                        Text(
+                            "RECIPES",
+                            color = myOrange
+                        )
+                    },
+                    selected = true,
+                    onClick = {
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+                NavigationDrawerItem(
+                    label = { Text("PRODUCTS") },
+                    selected = false,
+                    onClick = {
+                        scope.launch {
+                            navController.navigate(ProductMainScreenRoute)
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+                NavigationDrawerItem(
+                    label = { Text("CARTS") },
+                    selected = false,
+                    onClick = {
+                        scope.launch {
+                            navController.navigate(CartScreenRoute)
+                            drawerState.close()
+                        }
+                    },
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "Profile")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Drawer"
+                            )
+                        }
+                    },
+
+                    )
+            }
+        ) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) { }
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/myreceipeapp/persentation/screens/auth/SignUp/SignUpViewModel.kt.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/auth/SignUp/SignUpViewModel.kt.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myreceipeapp.domain.Repository.Auth.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
