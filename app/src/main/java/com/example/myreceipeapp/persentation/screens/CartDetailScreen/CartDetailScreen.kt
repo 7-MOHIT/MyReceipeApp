61fd178c3e7feb5d839271b7c509481c9e6b3c8c
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-14 21:57:59 +0530

    made a CartDetailScreen.kt , and its vM CartDetailScreenViewModel.kt

---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartDetailScreen/CartDetailScreen.kt
@@ -0,0 +1,8 @@
+package com.example.myreceipeapp.persentation.screens.CartDetailScreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CartDetailScreen() {
+
+}
---
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartDetailScreen/CartDetailScreenViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.myreceipeapp.persentation.screens.CartDetailScreen
+
+import androidx.lifecycle.ViewModel
+
+class CartDetailScreenViewModel: ViewModel() {
+}
