90cdf2569f2f45eb669f12061d96c73b9e98d643
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-15 20:36:26 +0530

    created a CartApiService.kt

--- app/src/main/java/com/example/myreceipeapp/data/remote/CartApiService.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/CartApiService.kt
@@ -1,5 +1,6 @@
 package com.example.myreceipeapp.data.remote
 
+import com.example.myreceipeapp.data.remote.dto.Carts.Cart
 import com.example.myreceipeapp.data.remote.dto.Carts.CartResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -8,5 +9,8 @@ import io.ktor.client.request.get
 class CartApiService(private val client: HttpClient) {
     suspend fun getAllCarts(): CartResponse {
         return client.get(urlString = "${KtorClient.CARTS}").body()
+    }
+    suspend fun getCartById(id: Int): Cart {
+        return client.get(urlString = "${KtorClient.CARTS}/$id").body()
     }
 }
