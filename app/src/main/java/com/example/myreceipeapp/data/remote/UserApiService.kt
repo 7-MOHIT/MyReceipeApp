1b53cd0eeae321547acbd709c5761aea9f536ca3
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-16 13:45:03 +0530

    created a UserApiService.kt file fetched the users api. from ktorClient.

--- app/src/main/java/com/example/myreceipeapp/data/remote/UserApiService.kt
+++ app/src/main/java/com/example/myreceipeapp/data/remote/UserApiService.kt
@@ -1,4 +1,12 @@
 package com.example.myreceipeapp.data.remote
 
-class UserApiService {
+import com.example.myreceipeapp.data.remote.dto.User.User
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class UserApiService(private val client: HttpClient) {
+    suspend fun getUserById(id: Int): User {
+        return client.get(urlString = "${KtorClient.USERS}/$id").body()
+    }
 }
