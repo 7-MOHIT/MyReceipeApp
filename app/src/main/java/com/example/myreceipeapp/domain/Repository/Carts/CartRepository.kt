a5a412e191587bd7450f88f6049c1612077a0169
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-17 03:34:55 +0530

    getCartWithUser fun created.

--- app/src/main/java/com/example/myreceipeapp/domain/Repository/Carts/CartRepository.kt
+++ app/src/main/java/com/example/myreceipeapp/domain/Repository/Carts/CartRepository.kt
@@ -3,8 +3,8 @@ package com.example.myreceipeapp.domain.Repository.Carts
 import com.example.myreceipeapp.data.remote.dto.Carts.Cart
 import com.example.myreceipeapp.data.remote.dto.Carts.CartWithUser
 
-interface CartRepository  {
-    suspend fun getAllCarts():List<Cart>
+interface CartRepository {
+    suspend fun getAllCarts(): List<Cart>
     suspend fun getCartById(id: Int): Cart
     suspend fun getCartWithUser(cartId: Int): CartWithUser
 }
