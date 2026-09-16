7d7dab839fa86823c79ad94b7c0696975a1f3e71
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-16 13:44:28 +0530

    adding the in getCartWithUser fun in CartRepository.kt file

--- app/src/main/java/com/example/myreceipeapp/domain/Repository/Carts/CartRepository.kt
+++ app/src/main/java/com/example/myreceipeapp/domain/Repository/Carts/CartRepository.kt
@@ -1,8 +1,10 @@
 package com.example.myreceipeapp.domain.Repository.Carts
 
 import com.example.myreceipeapp.data.remote.dto.Carts.Cart
+import com.example.myreceipeapp.data.remote.dto.Carts.CartWithUser
 
 interface CartRepository  {
     suspend fun getAllCarts():List<Cart>
     suspend fun getCartById(id: Int): Cart
+    suspend fun getCartWithUser(cartId: Int): CartWithUser
 }
