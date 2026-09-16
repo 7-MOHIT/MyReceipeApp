95aa5d28fa37389fef6894c78214249eee079d13
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-16 13:01:29 +0530

    created a viewModel for the cartScreen and  writing the fetchCartById fun. and using the coroutines.

--- app/src/main/java/com/example/myreceipeapp/persentation/screens/CartDetailScreen/CartDetailScreenViewModel.kt
+++ app/src/main/java/com/example/myreceipeapp/persentation/screens/CartDetailScreen/CartDetailScreenViewModel.kt
@@ -1,6 +1,41 @@
 package com.example.myreceipeapp.persentation.screens.CartDetailScreen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myreceipeapp.data.remote.CartApiService
+import com.example.myreceipeapp.data.remote.KtorClient
+import com.example.myreceipeapp.data.remote.dto.Carts.Cart
+import com.example.myreceipeapp.data.repository.CartRepositoryImpl
+import com.example.myreceipeapp.domain.Repository.Carts.CartRepository
+import kotlinx.coroutines.launch
 
-class CartDetailScreenViewModel: ViewModel() {
+class CartDetailScreenViewModel : ViewModel() {
+    private val repository: CartRepository =
+        CartRepositoryImpl(apiService = CartApiService(KtorClient.client))
+
+    var isLoading by mutableStateOf(false)
+        private set
+
+    var errorMessage by mutableStateOf<String?>(null)
+        private set
+
+    var cart by mutableStateOf<Cart?>(null)
+        private set
+
+    fun fetchCartById(cartId: Int) {
+        isLoading = true
+        errorMessage = null
+        viewModelScope.launch {
+            try {
+                cart = repository.getCartById(cartId)
+            } catch (e: Exception) {
+                errorMessage = e.message ?: "An unexpected error occurred."
+            } finally {
+                isLoading = false
+            }
+        }
+    }
 }
