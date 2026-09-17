5d5d677e2309d488ce610a616ec0478d0e72150d
MOHIT JANGRA <24001001083mohit@gmail.com>
2026-09-17 13:45:27 +0530

    getting the implementations of the getCartWithUser fun in the CartRepositoryImpl.kt file.

--- app/src/main/java/com/example/myreceipeapp/data/repository/CartRepositoryImpl.kt
+++ app/src/main/java/com/example/myreceipeapp/data/repository/CartRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.myreceipeapp.data.repository
 
 import com.example.myreceipeapp.data.remote.CartApiService
 import com.example.myreceipeapp.data.remote.dto.Carts.Cart
+import com.example.myreceipeapp.data.remote.dto.Carts.CartWithUser
 import com.example.myreceipeapp.domain.Repository.Carts.CartRepository
 
 class CartRepositoryImpl(private val apiService: CartApiService) : CartRepository {
@@ -11,5 +12,9 @@ class CartRepositoryImpl(private val apiService: CartApiService) : CartRepositor
 
     override suspend fun getCartById(id: Int): Cart {
         return apiService.getCartById(id)
+    }
+
+    override suspend fun getCartWithUser(cartId: Int): CartWithUser {
+        return apiService.getUserById(cartId)
     }
 }
